b3dcb454c26b9c6065dff68ecfae077b6af05b00
Edu-Lu <moralesluis17106494@gmail.com>
2022-08-02 11:29:11 -0500

    Se agrego la Clase 022 Problema 1, 2 ,3 y 4 Resueltos

--- app/src/main/java/com/luismorales17106494/a000ejeciciosdekotlinya/Clase022Funciones.kt
+++ app/src/main/java/com/luismorales17106494/a000ejeciciosdekotlinya/Clase022Funciones.kt
@@ -39,4 +39,90 @@ class Clase022Funciones {
             print("$it ")
         }
     }
+
+    fun problema2() {
+        /*
+        Problema 2
+        Se desea almacenar los sueldos de operarios.
+        Cuando se ejecuta el programa se debe pedir la cantidad de sueldos a ingresar.
+        Luego crear un arreglo con dicho tamaño.
+        Definir una función de carga y otra de impresión.
+         */
+        println("Cuantos sueldo desea cargar: ")
+        val cantidad = readLine()!!.toInt()
+        val sueldos = IntArray(cantidad)
+        cargaElementosProb2(sueldos)
+        imprimirElementosProb2(sueldos)
+
+    }
+
+    private fun cargaElementosProb2(sueldos: IntArray) {
+        sueldos.forEachIndexed { index, elemento ->
+            println("Ingrese el sueldo: ")
+            sueldos[index] = readLine()!!.toInt()
+        }
+    }
+
+    private fun imprimirElementosProb2(sueldos: IntArray) {
+        sueldos.forEach {
+            println(it)
+        }
+    }
+
+    fun ejercicio1() {
+        /*
+        Ejercicio 1
+        Desarrollar un programa que permita ingresar un arreglo de n elementos,
+        ingresar n por teclado.
+        Elaborar dos funciones una donde se lo cree y cargue al arreglo
+        y otra que sume todos sus elementos y retorne dicho valor a la main
+        donde se lo imprima.
+         */
+        val arreglo = cargarElementosEjer1()
+        val suma = sumaDeElementosEjer1(arreglo)
+        println("La suma es : $suma")
+    }
+
+    private fun cargarElementosEjer1(): IntArray {
+        println("Ingrese el número de elementos a ingresar: ")
+        val cantidad = readLine()!!.toInt()
+        val arreglo = IntArray(cantidad)
+
+        for (index in arreglo.indices) {
+            println("Ingrese el elemento: ")
+            arreglo[index] = readLine()!!.toInt()
+        }
+        return arreglo
+
+    }
+
+    private fun sumaDeElementosEjer1(arreglo: IntArray): Int {
+        var suma = 0
+        for (index in arreglo.indices) {
+            suma += arreglo[index]
+        }
+        return suma
+
+    }
+
+    fun ejercicio2() {
+        /*
+        Ejercicio 2
+        Cargar un arreglo de n elementos. Imprimir el menor elemento
+         */
+        val arreglo = cargarElementosEjer1()
+        val elementoMenor = elementoMenorEjer2(arreglo)
+        println("El elemento menor es: $elementoMenor")
+    }
+
+    private fun elementoMenorEjer2(arreglo: IntArray): Any {
+        var menor = arreglo[0]
+        for (index in arreglo.indices) {
+            if (menor > arreglo[index]) menor = arreglo[index]
+
+
+        }
+        return menor
+
+    }
 }
--- app/src/main/java/com/luismorales17106494/a000ejeciciosdekotlinya/main.kt
+++ app/src/main/java/com/luismorales17106494/a000ejeciciosdekotlinya/main.kt
@@ -4,5 +4,10 @@ fun main() {
 
     //022 - Funciones: parámetros y retorno de datos tipo arreglo
     val clase002 = Clase022Funciones()
-    clase002.problema1()
+    //clase002.problema1() //Problema 1
+    //clase002.problema2() //Problema 2
+    //clase002.ejercicio1() //Ejercicio 1
+    clase002.ejercicio2() //Ejercicio 2
+
+
 }
